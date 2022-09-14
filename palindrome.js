0aa1174ce3265a507d6af81175d5ae6ce0122df7
unknown <you@example.com>
2022-09-14 16:51:24 +0530

    i have written the code to check palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,9 @@
-let ="masai";
+let str ="masai";
+let bag ="";
+for(let  i=str.lenth-1;i>=0;i--){
+    bag=bag+str[i];
+}if(bag===str){
+    console.log("palindrome");
+}else{
+    console.log("not a palindrome");
+}
